a7b9b0bd2a1bd6932a6be6a7f7206a50ed821482
Adrian Walker <awalker3365@gmail.com>
2021-02-03 15:52:12 -0500

    node calc

---
+++ calculator.js
@@ -0,0 +1,19 @@
+var num1 = prompt
+
+const readline = require("readline-sync");
+function func1(num1, num2) {
+    console.log(num1 + num2)
+};
+
+function func2(num1, num2) {
+    console.log(num1 * num2)
+}
+
+function func3(num1, num2) {
+    console.log(num1 / num2)
+}
+
+prompt('Please enter your first number:')
+console.log()
+
+prompt('Please enter your second number')
